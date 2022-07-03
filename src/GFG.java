790c675058f3e47320117169246d30bb91b1cc41
marib <khan.marib@gmail.com>
2022-07-03 14:46:48 -0400

    first commit

--- src/GFG.java
+++ src/GFG.java
@@ -1,6 +1,6 @@
 public class GFG {
     public static void main(String[] args) throws Exception {
-        System.out.println("Checking git repo");
+        System.out.println("hello, World");
 
     }
 }
