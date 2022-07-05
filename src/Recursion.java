833a9d93bc20a7d0e4d3b5cc0e191b03d559acd7
marib <khan.marib@gmail.com>
2022-07-04 21:22:24 -0400

    recursion

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,14 +1,24 @@
 public class Recursion {
 
-    public static void fun(int n){
+    public static void printNto1(int n){
         if (n <= 0){
             return;
         }
         System.out.println(n);
-        fun(n-1);
+        printNto1(n-1);
     }
+
+    public static void print1toN(int n){
+        if ( n== 0){
+            return;
+        }
+        print1toN(n-1);
+        System.out.println(n);
+    }
+
+
     public static void main(String[] args) throws Exception {
-        fun(20);
+        print1toN(5);
 
     }
 }
